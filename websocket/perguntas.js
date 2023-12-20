17b56ba7bf68462b057928f90922bcf0ca4a96a6
pedrords3 <42254602+pedrords3@users.noreply.github.com>
2023-12-20 16:47:43 -0300

    Add files via upload

---
+++ websocket/perguntas.js
@@ -0,0 +1,10 @@
+const perguntas = [
+    {
+        pergunta: "Qual é a capital do Brasil?",
+        opcoes: ["Rio de Janeiro", "Brasília", "São Paulo", "Salvador"],
+        resposta: "Brasília"
+    },
+    // Adicione mais perguntas conforme necessário
+];
+
+module.exports = perguntas;
