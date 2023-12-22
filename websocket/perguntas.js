05e1728e99fad7111118e57cb5d134adbacc1a7f
pedrords3 <42254602+pedrords3@users.noreply.github.com>
2023-12-22 11:48:01 -0300

    Add files via upload

--- websocket/perguntas.js
+++ websocket/perguntas.js
@@ -1,18 +1,16 @@
 const perguntas = [
     {
         pergunta: "Qual é a capital do Brasil?",
-        opcoes: ["Rio de Janeiro", "Brasília", "São Paulo", "Salvador"],
-        resposta: "Brasília"
+        // opcoes: ["Rio de Janeiro", "Brasília", "São Paulo", "Salvador"],
+        // resposta: "Brasília"
     },
     {
         pergunta: "Nome do filme do ano?",
-        opcoes: ["Rio de Janeiro", "Brasília", "São Paulo", "Salvador"],
-        resposta: "Brasília"
+
     },
     {
         pergunta: "Pergunta pipipi popopo?",
-        opcoes: ["Rio de Janeiro", "Brasília", "São Paulo", "Salvador"],
-        resposta: "Brasília"
+
     },
     // Adicione mais perguntas conforme necessário
 ];
