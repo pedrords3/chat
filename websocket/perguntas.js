bd4cdd3122f815ac4a512551b583b5e611bd81a4
pedrords3 <42254602+pedrords3@users.noreply.github.com>
2023-12-22 22:03:30 -0300

    Add files via upload

--- websocket/perguntas.js
+++ websocket/perguntas.js
@@ -6,11 +6,24 @@ const perguntas = [
     },
     {
         pergunta: "Nome do filme do ano?",
-
     },
     {
         pergunta: "Pergunta pipipi popopo?",
-
+    },
+    {
+        pergunta: "_____: TESTADO PELAS CRIANÇAS, APROVADO PELAS MÃES.",
+    },
+    {
+        pergunta: "ELE PERGUNTOU: NO CÉU TEM ____? E MORREU.",
+    },
+    {
+        pergunta: "NO FUTURO DISTANTE, HISTORIADORES IRÃO CONCORDAR QUE  _____ MARCOU O INÍCIO DO DECLÍNIO DA AMÉRICA.",
+    },
+    {
+        pergunta: "QUAL É O PRÓXIMO BRINQUEDO DO MCLANCHE FELIZ?",
+    },
+    {
+        pergunta: "NO PRÓXIMO NATAL O PAPAI NOEL VAI DAR ____ PRA CRIANÇA QUE NÃO SE COMPORTAR.",
     },
     // Adicione mais perguntas conforme necessário
 ];
